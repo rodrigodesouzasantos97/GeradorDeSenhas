ba7836362bea6a092089d1d0e74e3d64c7a1b8c2
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-02-01 15:53:15 -0300

    Added new elements and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,71 @@
 const generatePasswordButton = document.querySelector("#generate-password-btn");
 const passwordContainer = document.querySelector("#password-container");
 const passwordElement = document.querySelector("#password");
+const lengthInput = document.querySelector("#length-input");
+const lettersInput = document.querySelector("#letters-input");
+const numbersInput = document.querySelector("#numbers-input");
+const symbolsInput = document.querySelector("#symbols-input");
+const copyPasswordBtn = document.querySelector("#copy-password-btn");
 
-function getLetterLowerCase(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function getLetterLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function getLetterUpperCase(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function getLetterUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getNumber(){
-    return Math.floor(Math.random() * 10).toString();
+function getNumber() {
+  return Math.floor(Math.random() * 10).toString();
 }
 
-function getSymbol(){
-    const symbols = "!@#$%&*()_+-=?/[]{},.";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+function getSymbol() {
+  const symbols = "!@#$%&*()_+-=?/[]{},.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-function generatePassword(){
-    let password = "";
-    const passwordLength = 10;
+function generatePassword() {
+  let password = "";
+  const passwordLength = lengthInput.value;
 
-    const generatos = [getLetterLowerCase(), getLetterUpperCase(), getNumber(), getSymbol()];
+  const generatos = [];
 
-    for(i = 0; i < passwordLength; i++){
-        const randomValue = generatos[Math.floor(Math.random() * generatos.length)];
-        password += randomValue;
-    }
+  if (lettersInput.checked)
+    generatos.push(getLetterLowerCase, getLetterUpperCase);
 
-    passwordElement.innerText = password;
-    passwordContainer.classList.remove("hide");
+  if (numbersInput.checked) generatos.push(getNumber);
+
+  if (symbolsInput.checked) generatos.push(getSymbol);
+
+  if (generatos.length === 0) return;
+
+  for (i = 0; i < passwordLength; i++) {
+    const randomValue =
+      generatos[Math.floor(Math.random() * generatos.length)]();
+    password += randomValue;
+  }
+
+  passwordElement.innerText = password;
+  passwordContainer.classList.remove("hide");
 }
 
-generatePasswordButton.addEventListener("click", () =>{
-    generatePassword();
-})
+function copyValue(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    copyPasswordBtn.innerText = "Copiado!";
+    copyPasswordBtn.classList.add("copyed")
+    
+    setTimeout(() => {
+      copyPasswordBtn.innerText = "Copiar";
+      copyPasswordBtn.classList.remove("copyed")
+    }, 1000);
+  });
+}
+
+generatePasswordButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  generatePassword();
+});
+
+copyPasswordBtn.addEventListener("click", () => {
+  copyValue(passwordElement.innerText);
+});
